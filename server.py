e648cfa803628a61538eff062842f7ac60956668
Carlos Valera <valera@google.com>
2014-08-26 19:29:19 -0400

    Added MIT license

--- server.py
+++ server.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python2
-
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Carlos Valera
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#
 # Single-file HTTP server for uploading files. Meant to be a quick and dirty
 # solution to transfering files on a local network. Uses just the python2.7
 # stdlib.
-
+#
 # OUTSTANDING ISSUES
 # ==================
-# + No OSS license yet. Plan to add MIT license. Don't have access to internet
-#   right now to do so.
 # + Interfaces w/ OS don't catch errors, so, for example, if this fails to save
 #   a file, it'll kill the application.
 # + Should add a flag to denote which directory to save files in, rather than
