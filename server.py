b469277e4d84440ebf98455939710f2c1e2d9e31
Carlos Valera <valera@google.com>
2014-08-26 14:44:34 -0400

    Complete rewrite

--- server.py
+++ server.py
@@ -1,123 +1,114 @@
 #!/usr/bin/python2
 
-# Copyright Jon Berg , turtlemeat.com
-# Modified by nikomu @ code.google.com
-# Modified by Carlos V. <cookyt2@gmail.com>
-
-import cgi
 import HTMLParser
+import cgi
+import httplib
+import logging
 import os
 import sys
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from wsgiref.simple_server import make_server
 
-class PostError(Exception):
-  pass
 
-class GetError(Exception):
-  pass
+kFileFieldName = 'upfile'
 
-class UploadHandler(BaseHTTPRequestHandler):
-  def __init__(self, *args, **kwargs):
-    self.file_field_name = 'upfile'
-    BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
 
-  def write_upload_form(self, uploaded_file_name=None):
-    def uploaded_message():
-      if uploaded_file_name == None:
-        return ''
-      else:
-        return ('<p><em>File uploaded under name: {0}</em></p>'
-               .format(cgi.escape(uploaded_file_name)))
+def SendUploadForm(response, uploaded_file_name=None):
+  response('200 OK', [('Content-Type','text/html')])
 
-    form = """
-      <html>
-        <body>
-          <h1>Upload a file</h1>
-          {0}
-          <form method='post' enctype='multipart/form-data' action='/'>
-            <p><input type="file" name="{1}" /></p>
-            <p><input type="submit" value="Upload" /></p>
-          </form>
-        </body>
-      </html>
-    """.format(uploaded_message(), self.file_field_name)
-    self.wfile.write(form)
+  uploaded_file_message = ''
+  if uploaded_file_name:
+    html_parser = HTMLParser.HTMLParser()
+    html_safe_filename = html_parser.unescape(uploaded_file_name)
+    uploaded_file_message = ('<p><em>File uploaded under name: {0}</em></p>'
+                             .format(html_safe_filename))
 
-  def send_html_response_ok(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/html')
-    self.end_headers()
+  return '''
+    <html>
+      <body>
+        <h1>Upload a file</h1>
+        {0}
+        <form method='post' enctype='multipart/form-data' action='/'>
+          <p><input type='file' name='{1}' /></p>
+          <p><input type='submit' value='Upload' /></p>
+        </form>
+      </body>
+    </html>
+  '''.format(uploaded_file_message, kFileFieldName)
 
-  def do_GET(self):
-    try:
-      if self.path == '/' :
-        self.send_html_response_ok()
-        self.write_upload_form()
-      else:
-        raise GetError('Requested non-root path: {0}'.format(self.path))
-    except Exception as e :
-      sys.stderr.write(str(e) + '\n')
-      self.send_error(404, 'File Not Found: {0}'.format(self.path))
 
-  def do_POST(self):
-    def get_unique_local_filename(filename):
-      cwd = os.path.abspath('.')
-      filename = os.path.split(filename)[1] # strip the path
-      local_filename = os.path.join(cwd, filename)
+def SendErrorPage(response, error_code, message):
+  error_message = httplib.responses[error_code]
+  error_status = '%d %s' % (error_code, error_message)
+  headers = [('Content-Type','text/html')]
+  response(error_status, headers)
+  return """
+    <html>
+      <body>
+        <h1>{0}</h1>
+        <p>{1}</p>
+      </body>
+    </html>
+  """.format(error_status, cgi.escape(message))
 
-      unique_filename = local_filename
-      i = 1
-      while os.path.exists(unique_filename):
-        unique_filename = "{0}-copy{1}".format(local_filename, i)
-        i += 1
 
-      return unique_filename
+def UniqueLocalFilename(filename):
+  filename = os.path.split(filename)[1]  # strip the path
+  local_filename = os.path.join(os.getcwd(), filename)
 
-    def save_file(filename, data):
-      sys.stderr.write("Saving file `{0}'... ".format(filename))
-      with open(filename, 'wb') as outfile:
-        outfile.write(data.read())
-      sys.stderr.write("[saved]\n")
+  new_filename = local_filename
+  i = 0
+  while os.path.exists(new_filename):
+    new_filename = '%s-%d' % (local_filename, i)
+    i += 1
 
-    def send_response(filename):
-      self.send_response(200)
-      self.end_headers()
-      self.write_upload_form(os.path.split(filename)[1])
+  return new_filename
 
-    try:
-      content_type, options = cgi.parse_header(self.headers['content-type'])
-      if content_type == 'multipart/form-data':
-        # Most of the needed environment variables will be read, instead, from
-        # the 'headers' object, but as the FieldStorage object was designed for
-        # CGI, absense of 'POST' value in environ will prevent the object from
-        # using the 'fp' argument
-        fields = cgi.FieldStorage(fp = self.rfile,
-                                  headers = self.headers,
-                                  environ={'REQUEST_METHOD': 'POST'})
-      else:
-        raise PostError("Content is not `multipart/form-data'. "+
-                        "Got `{0}' instead".format(content_type))
 
-      upload_field = fields[self.file_field_name]
-      local_filename = get_unique_local_filename(upload_field.filename)
-      save_file(local_filename, upload_field.file)
-      send_response(local_filename)
+def SaveFile(filename, data):
+  logging.info("saving file %r", filename)
+  with open(filename, 'wb') as outfile:
+    outfile.write(data.read())
+  logging.info("saved file as %r", filename)
 
-    except Exception as e:
-      sys.stderr.write(str(e) + '\n')
-      self.send_error(404, 'Failed post request; path: {0}'.format(self.path))
+
+class UploaderApp(object):
+  def __call__(self, environ, response):
+    if environ['PATH_INFO'] != '/':
+      error_message = 'Not found: %s' % (environ['PATH_INFO'],)
+      return SendErrorPage(response, httplib.NOT_FOUND, error_message)
+
+    method_handler = getattr(self, environ['REQUEST_METHOD'],
+                             self.DefaultMethodHandler)
+    method_handler(environ, response)
+
+  def DefaultMethodHandler(self, environ, response):
+    error_message = 'Method not allowed: %s' % (environ['REQUEST_METHOD'],)
+    return SendErrorPage(response, httplib.METHOD_NOT_ALLOWED, error_message)
+
+  def POST(self, environ, response):
+    field_storage = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
+    if kFileFieldName not in field_storage:
+      return SendErrorPage(response, httplib.PRECONDITION_FAILED,
+                           'No file data sent.')
+    upload_field = field_storage[kFileFieldName]
+    filename = UniqueLocalFilename(upload_field.filename)
+    SaveFile(filename, upload_field.file)
+    return SendUploadForm(response, filename)
+
+  def GET(self, environ, response):
+    return SendUploadForm(response)
 
 def main():
+  logging.basicConfig(level=logging.INFO)
+
+  # TODO use argparse for arguments
   kDefaultPort= 8000
   port_number = int(sys.argv[1]) if len(sys.argv) > 1 else kDefaultPort
-  try:
-    server = HTTPServer(('', port_number), UploadHandler)
-    sys.stderr.write('starting HTTP file upload server on port {0}...\n'
-                     .format(port_number))
-    server.serve_forever()
-  except KeyboardInterrupt:
-    sys.stderr.write('shutting down server.\n')
+
+  httpd = make_server('', port_number, UploaderApp())
+  logging.info('starting HTTP file upload server on port %d', port_number)
+  httpd.serve_forever()
 
 if __name__ == '__main__':
   main()
